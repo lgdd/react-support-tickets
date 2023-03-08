09832d70f8f73cf68137be928545341452333b55
lgdd <louis-guillaume.durand@liferay.com>
2023-03-08 20:24:27 +0100

    chore: initial commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import LiferayService from './LiferayService';
+
+function App() {
+  const scopeKey = window['Liferay'].ThemeDisplay.getSiteGroupId() | 50123;
+  const data = LiferayService.get(`/o/c/supporttickets/scopes/${scopeKey}`);
+
+  const priorityCss = (priority) => {
+    if (priority === 'major') {
+      return 'label label-warning';
+    }
+    if (priority === 'critical') {
+      return 'label label-danger';
+    }
+    return 'label label-secondary';
+  };
+
+  const stateCss = (state) => {
+    if (state === 'open') {
+      return 'label label-success';
+    }
+    if (state === 'closed') {
+      return 'label label-danger';
+    }
+    return 'label label-info';
+  };
+
+  const viewTicket = (id) => {
+    window.location.href = `/web/member-portal/l/${id}`;
+  };
+
+  return (
+    <div className="App">
+      <table class="table table-autofit table-list table-nowrap table-responsive">
+        <thead>
+          <tr>
+            <th class="table-cell-expand">
+              <span class="text-truncate-inline">
+                <span class="text-truncate">Subject</span>
+              </span>
+            </th>
+            <th class="table-cell-expand">
+              <span class="text-truncate-inline">
+                <span class="text-truncate">State</span>
+              </span>
+            </th>
+            <th class="table-cell-expand">
+              <span class="text-truncate-inline">
+                <span class="text-truncate">Priority</span>
+              </span>
+            </th>
+            <th class="table-column-text-end"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.items.map((item) => {
+            return (
+              <tr>
+                <td class="table-cell-expand">
+                  <div class="table-list-title">
+                    <a class="text-truncate-inline" href="#1">
+                      <span class="text-truncate">{item.subject}</span>
+                    </a>
+                  </div>
+                </td>
+                <td class="table-cell-expand">
+                  <span class="text-truncate-inline">
+                    <span className={stateCss(item.state.key)}>
+                      <span class="label-item label-item-expand">
+                        {item.state.name}
+                      </span>
+                    </span>
+                  </span>
+                </td>
+                <td class="table-cell-expand">
+                  <span class="text-truncate-inline">
+                    <span className={priorityCss(item.priority.key)}>
+                      <span class="label-item label-item-expand">
+                        {item.priority.name}
+                      </span>
+                    </span>
+                  </span>
+                </td>
+                <td class="table-column-text-end">
+                  <div class="btn-group btn-group-nowrap">
+                    <div class="btn-group-item">
+                      <button
+                        class="btn btn-secondary btn-sm"
+                        type="button"
+                        onClick={() => {
+                          viewTicket(item.id);
+                        }}
+                      >
+                        View
+                      </button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LiferayService.js
@@ -0,0 +1,134 @@
+const LiferayService = {
+  get: async (url) => {
+    if (typeof window['Liferay'] != 'undefined') {
+      try {
+        const response = await window['Liferay'].Util.fetch(url);
+        const data = response.json();
+        if (data.status) {
+          window['Liferay'].Util.openToast({
+            title: data.status,
+            message: 'An error occured.',
+            type: 'danger',
+          });
+        } else {
+          window['Liferay'].Util.openToast({
+            title: 'Success',
+            message: `Your request completed successfully.`,
+            type: 'success',
+          });
+          return data;
+        }
+      } catch (error) {
+        console.error(error);
+        window['Liferay'].Util.openToast({
+          message: 'An error occured.',
+          type: 'danger',
+        });
+      }
+    } else {
+      console.error("Liferay (JS Object) doesn't exist.");
+    }
+  },
+  post: (url, payload) => {
+    if (typeof window['Liferay'] != 'undefined') {
+      try {
+        const response = window['Liferay'].Util.fetch(url, {
+          body: JSON.stringify(payload),
+          method: `POST`,
+          headers: [['content-type', 'application/json']],
+        });
+        const data = response.json();
+        if (data.status) {
+          window['Liferay'].Util.openToast({
+            title: data.status,
+            message: 'An error occured.',
+            type: 'danger',
+          });
+        } else {
+          window['Liferay'].Util.openToast({
+            title: 'Success',
+            message: `Your request completed successfully.`,
+            type: 'success',
+          });
+          return data;
+        }
+      } catch (error) {
+        console.error(error);
+        window['Liferay'].Util.openToast({
+          message: 'An error occured.',
+          type: 'danger',
+        });
+      }
+    } else {
+      console.error("Liferay (JS Object) doesn't exist.");
+    }
+  },
+  patch: (url, payload) => {
+    if (typeof window['Liferay'] != 'undefined') {
+      try {
+        const response = window['Liferay'].Util.fetch(url, {
+          body: JSON.stringify(payload),
+          method: `PATCH`,
+          headers: [['content-type', 'application/json']],
+        });
+        const data = response.json();
+        if (data.status) {
+          window['Liferay'].Util.openToast({
+            title: data.status,
+            message: 'An error occured.',
+            type: 'danger',
+          });
+        } else {
+          window['Liferay'].Util.openToast({
+            title: 'Success',
+            message: `Your request completed successfully.`,
+            type: 'success',
+          });
+          return data;
+        }
+      } catch (error) {
+        console.error(error);
+        window['Liferay'].Util.openToast({
+          message: 'An error occured.',
+          type: 'danger',
+        });
+      }
+    } else {
+      console.error("Liferay (JS Object) doesn't exist.");
+    }
+  },
+  delete: async (url) => {
+    if (typeof window['Liferay'] != 'undefined') {
+      try {
+        const response = await window['Liferay'].Util.fetch(url, {
+          method: `DELETE`,
+        });
+        const data = response.json();
+        if (data.status) {
+          window['Liferay'].Util.openToast({
+            title: data.status,
+            message: 'An error occured.',
+            type: 'danger',
+          });
+        } else {
+          window['Liferay'].Util.openToast({
+            title: 'Success',
+            message: `Your request completed successfully.`,
+            type: 'success',
+          });
+          return data;
+        }
+      } catch (error) {
+        console.error(error);
+        window['Liferay'].Util.openToast({
+          message: 'An error occured.',
+          type: 'danger',
+        });
+      }
+    } else {
+      console.error("Liferay (JS Object) doesn't exist.");
+    }
+  },
+};
+
+export default LiferayService;
